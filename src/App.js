08f9c501c5928aaa7fafcc45b9a6aebf9bac829d
ngriffith29 <nathangriffith@Nathans-MacBook-Pro.local>
2019-08-23 19:37:18 -0400

    finished project

--- src/App.js
+++ src/App.js
@@ -1,43 +1,39 @@
-//TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, { useState } from 'react';
 import "./App.css";
-import BottomRow from "./BottomRow";
 
-function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+function BottomRow  () {
+  const [Yard, setBallYard] = useState(20);
+  const countRef = Yard
+
+ 
+    setTimeout(() => {
+      setBallYard(countRef + 10);
+    }, 2000);
+ 
+
+
 
   return (
-    <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">32</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
-          </div>
-        </div>
-        <BottomRow />
-      </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
-        </div>
-      </section>
+    <div className="bottomRow">
+      <div className="down">
+        <h3 className="down__title">Down</h3>
+        <div className="down__value">3</div>
+      </div>
+      <div className="toGo">
+        <h3 className="toGo__title">To Go</h3>
+        <div className="toGo__value">7</div>
+      </div>
+      <div className="ballOn">
+        <h3 className="ballOn__title">Ball on</h3>
+        <div className="ballOn__value">{Yard}</div>
+        <button className="homeButtons__fieldGoal" onClick={() => setBallYard(Yard + 10)}>{Yard}</button>
+      </div>
+      <div className="quarter">
+        <h3 className="quarter__title">Quarter</h3>
+        <div className="quarter__value">4</div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default BottomRow;
